c65045dc53186435eb8a3a642b87886f15405f15
knavillus1 <120214046+knavillus1@users.noreply.github.com>
2025-05-23 02:11:34 -0600

    Add React Router and update dev script

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,20 @@
-import { useEffect, useState } from 'react'
 import LandingPage from './pages/LandingPage'
 import MainLayout from './layouts/MainLayout'
 import ProjectDetailPage from './pages/ProjectDetailPage'
+import { Routes, Route, useParams } from 'react-router-dom'
+
+function ProjectDetailWrapper() {
+  const { projectId } = useParams()
+  return <ProjectDetailPage projectId={projectId || ''} />
+}
 
 export default function App() {
-  const [path, setPath] = useState(window.location.pathname)
-
-  useEffect(() => {
-    const onPop = () => setPath(window.location.pathname)
-    window.addEventListener('popstate', onPop)
-    return () => window.removeEventListener('popstate', onPop)
-  }, [])
-
-  let content
-  if (path.startsWith('/project/')) {
-    const projectId = path.replace('/project/', '')
-    content = <ProjectDetailPage projectId={projectId} />
-  } else {
-    content = <LandingPage />
-  }
-
-  return <MainLayout>{content}</MainLayout>
+  return (
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/project/:projectId" element={<ProjectDetailWrapper />} />
+      </Routes>
+    </MainLayout>
+  )
 }
